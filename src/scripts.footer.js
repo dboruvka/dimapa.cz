ecbb5b329b2107016091aeac669d6edd79f6b2b3
dboruvka <david.boruvka@gmail.com>
2023-08-29 19:23:05 +0200

    oprava pro kliknutí na mobilu

--- src/scripts.footer.js
+++ src/scripts.footer.js
@@ -1,20 +1,58 @@
 $(document).ready(function() {
 
-  $(".custom-footer__banner22").insertAfter( $("body"));
+    $(".custom-footer__banner22").insertAfter( $("body"));
+    var breakpoint = document.documentElement.clientWidth;
 
-         $('.custom-footer__banner22').mouseenter(function() {
-    
-            $('.custom-footer__banner22').mouseenter(function() {
-                $(this).animate({
-                    left: '0'
-                },320);
-            });
+    if (breakpoint > 768)  // desktop
+    {
+                
 
-            $('.custom-footer__banner22 ').mouseleave(function() {
+                        //    $('.custom-footer__banner22').mouseenter(function() {
+                        $('.custom-footer__banner22').on('mouseenter', function() {
+                            $(this).animate({
+                                left: '0'
+                            },100);
+                        });
+
+                $('.custom-footer__banner22').on('mouseleave click', function() {
+                    //$('.custom-footer__banner22').on('touchend', function() {
+
+                    //    $('.custom-footer__banner22').mouseleave(function() {
                 $(this).animate({
-                    left: '-320px'
-                }, 200);
-            });
+                                left: '-320px'
+                            }, 200);
+                    
+                });
+
+    }
+
+else // mobil
+
+{
+
+    var zobrazeno = false;
+
+
+    $('.custom-footer__banner22').on('click', function() {
+
+        zobrazeno = !zobrazeno;
+
+        // Zobrazení nebo skrytí obrázku
+        if (zobrazeno) {
+          $(this).animate({
+            left: '0'
+          }, 100);
+        } else {
+          $(this).animate({
+            left: '-320px'
+          }, 200);
+        }
+              
+
       });
+
+}
+
+
     });
       
